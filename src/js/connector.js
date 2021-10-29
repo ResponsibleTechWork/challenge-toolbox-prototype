ef1cbe2b3dad7862a64f6901f92c8fb81241390d
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-29 13:59:56 +0100

    console log object data

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,29 +2,28 @@ console.log('testing challenge cards')
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
-    console.log('Someone clicked the button');
+    console.log('Someone clicked the button: ', t);
   };
   
   window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
+      console.log('card-buttons t object:', t);
       return [{
-        // usually you will provide a callback function to be run on button click
-        // we recommend that you use a popup on click generally
-        icon: GRAY_ICON, // don't use a colored icon here
+        icon: GRAY_ICON,
         text: 'Pop up challenges',
         callback: onBtnClick,
         condition: 'edit'
       }, {
-        // but of course, you could also just kick off to a url if that's your thing
         icon: GRAY_ICON,
         text: 'Just a URL',
         condition: 'always',
         url: 'https://www.the-public-good.com/',
-        target: 'The Public Good' // optional target for above url
+        target: 'The Public Good'
       }];
     },
   
     "card-badges": function (t, opts) {    
+      console.log('card-badges t object:', t);
       return t
         .card("name")
         .get("name")
