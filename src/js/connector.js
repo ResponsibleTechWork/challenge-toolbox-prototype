ac0360e7586f104173c4ead10ee00d3e17903ed2
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-30 16:19:32 +0100

    board data

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,10 +1,7 @@
 console.log('testing challenge cards')
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-var onBtnClick = function (t, opts) {
-    t.member("all").then(function (member) {
-        console.log(JSON.stringify(member, null, 2));
-      });
+var onBtnClick = function (t, opts) {    
     return t.card('id', 'name').then(function (card) {
         console.log(JSON.stringify(card, null, 2));
     });
@@ -12,36 +9,44 @@ var onBtnClick = function (t, opts) {
 
   window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
-      return [{
-        icon: GRAY_ICON,
-        text: 'Add challenge--',
-        callback: onBtnClick,
-        condition: 'edit'
-      }, {
-        icon: GRAY_ICON,
-        text: 'Just a URL',
-        condition: 'always',
-        url: 'https://www.the-public-good.com/',
-        target: 'The Public Good'
-      }];
-    },
+
+        t.board('id', 'name').then(function (card) {
+            console.log(JSON.stringify(card, null, 2));
+        });
+        t.member('id', 'username').then(function (member) {
+            console.log(JSON.stringify(member, null, 2));
+        });
+
+        return [{
+            icon: GRAY_ICON,
+            text: 'Add challenge--',
+            callback: onBtnClick,
+            condition: 'edit'
+        }, {
+            icon: GRAY_ICON,
+            text: 'Just a URL',
+            condition: 'always',
+            url: 'https://www.the-public-good.com/',
+            target: 'The Public Good'
+        }];
+        },
   
     "card-badges": function (t, opts) {    
-      return t
-        .card("name")
-        .get("name")
-        .then(function (cardName) {        
-          return [
-            {
-              text: "Challenge",
-              color: "red"
-            },
-            {
-              text: "Celebrate",
-              color: "green"
-            },
-          ];
-        });
-    },
+        return t
+            .card("name")
+            .get("name")
+            .then(function (cardName) {        
+            return [
+                {
+                text: "Challenge",
+                color: "red"
+                },
+                {
+                text: "Celebrate",
+                color: "green"
+                },
+            ];
+            });
+        },
   
   });
