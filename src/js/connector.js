970370b67cf1a2195b0600d34e2db680b1312af6
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-30 16:02:37 +0100

    onclick card data

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,20 +2,13 @@ console.log('testing challenge cards')
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
-    t.getAll()
-            .then(function (data) {
-                console.log('Clicked Add challenge button--');
-                console.log(JSON.stringify(data, null, 2));
-        });
+    return t.card("all").then(function (card) {
+        console.log(JSON.stringify(card, null, 2));
+    });
   };
-  
+
   window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
-        t.getAll()
-            .then(function (data) {
-                console.log('card-buttons');
-                console.log(JSON.stringify(data, null, 2));
-        });
       return [{
         icon: GRAY_ICON,
         text: 'Add challenge--',
@@ -31,11 +24,6 @@ var onBtnClick = function (t, opts) {
     },
   
     "card-badges": function (t, opts) {    
-        t.getAll()
-        .then(function (data) {
-            console.log('card-badges');
-            console.log(JSON.stringify(data, null, 2));
-    });
       return t
         .card("name")
         .get("name")
