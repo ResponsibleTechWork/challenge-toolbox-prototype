b6efd0b4c46fd25ab68256aa726e81fb58666892
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-30 16:31:33 +0100

    inline logging

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,22 +1,14 @@
-console.log('testing challenge cards')
+
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {    
-    return t.card('id', 'name').then(function (card) {
-        console.log(JSON.stringify(card, null, 2));
+    return t.getAll().then(function (all) {
+        console.log(JSON.stringify(all, null, 2));
     });
   };
 
   window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
-
-        t.board('id', 'name').then(function (card) {
-            console.log(JSON.stringify(card, null, 2));
-        });
-        t.member('id', 'username').then(function (member) {
-            console.log(JSON.stringify(member, null, 2));
-        });
-
         return [{
             icon: GRAY_ICON,
             text: 'Add challenge--',
@@ -33,8 +25,10 @@ var onBtnClick = function (t, opts) {
   
     "card-badges": function (t, opts) {    
         return t
-            .card("name")
-            .get("name")
+            .card('id', 'name')
+            .then(function (card) {
+                console.log(JSON.stringify(card, null, 2));
+            })
             .then(function (cardName) {        
             return [
                 {
@@ -49,4 +43,15 @@ var onBtnClick = function (t, opts) {
             });
         },
   
+    // 'card-buttons': function (t, opts) {
+    //     return t.board('id', 'name').then(function (card) {
+    //         console.log(JSON.stringify(card, null, 2));
+    //     });
+    //     },
+  
+    // 'card-buttons': function (t, opts) {
+    //     return t.member('id', 'username').then(function (member) {
+    //         console.log(JSON.stringify(member, null, 2));
+    //     });
+    //     },
   });
