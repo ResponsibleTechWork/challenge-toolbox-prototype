063588ce065bcbe85cea9c445549cac0cd108a6e
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-30 18:08:34 +0100

    add card-back-section

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,10 +4,29 @@ const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-
 const onBtnClick = function (t, opts) {    
     const context = t.getContext();
     console.log(JSON.stringify(context, null, 2));
-    return t.card('id', 'name').then(function (card) {
-        console.log('click event, card');
-        console.log(JSON.stringify(card, null, 2));
-    });
+
+
+    window.TrelloPowerUp.initialize({
+        'card-back-section': function(t, options){
+          return {
+            title: 'My Card Back Section',
+            icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+            content: {
+              type: 'iframe',
+              url: t.signUrl('./section.html'),
+              height: 230, // Max height is 1500.
+              action: {
+                text: 'My Action',
+                callback: (t) => t.popup(),
+              },
+            }
+          };
+        }
+      });
+    // return t.card('id', 'name').then(function (card) {
+    //     console.log('click event, card');
+    //     console.log(JSON.stringify(card, null, 2));
+    // });
   };
 
   window.TrelloPowerUp.initialize({
