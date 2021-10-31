43c9a7897e2e611b0192644840d87002abf085e4
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-31 11:59:46 +0000

    card-detail-badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -8,41 +8,16 @@ const onBtnClick = function (t, opts) {
     t.get('card', 'shared', 'isClicked').then(function(data) {
         console.log('card clicked callback, properties of isClicked: ', JSON.stringify(data, null, 2));
     });
-
-    
-    // return {'card-back-section': function(cbs, options){
-    //     return {
-    //     title: 'My Card Back Section',
-    //     icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-    //     content: {
-    //         type: 'iframe',
-    //         url: cbs.signUrl('../html/section.html'),
-    //         height: 230, // Max height is 1500.
-    //         action: {
-    //             text: 'My Action',
-    //             // callback: (t) => t.popup(),
-    //         },
-    //     }
-    //     };
-    // }}
-      
-    // return t.card('id', 'name').then(function (card) {
-    //     console.log('click event, card');
-    //     console.log(JSON.stringify(card, null, 2));
-    // });
   };
 
   window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
         const context = t.getContext();
         // console.log(JSON.stringify(context, null, 2));
-
         const myKeyValueObject = {
             'isClicked': 'you bectcha!',
           };
         t.set('card', 'shared', myKeyValueObject);
-
-
         return [{
             icon: GRAY_ICON,
             text: 'Add challenge--',
@@ -56,7 +31,6 @@ const onBtnClick = function (t, opts) {
             target: 'The Public Good'
         }];
         },
-  
     "card-badges": function (t, opts) {    
         return t
             .card('id', 'name')            
@@ -73,20 +47,43 @@ const onBtnClick = function (t, opts) {
             ];
             });
         },
-    'card-back-section': function(cbs, options){
-        return {
-        title: 'My Card Back Section',
-        icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-        content: {
-            type: 'iframe',
-            url: cbs.signUrl('/src/html/section.html', { anArg: 'hey' }),
-            height: 230, // Max height is 1500.
-            action: {
-                text: 'My Action',
-                callback: (t) => t.popup(),
-            },
-        }
-        };
-    }
+        "card-detail-badges": function (t, opts) {
+            return t
+              .card("name")
+              .get("name")
+              .then(function (cardName) {
+                console.log("We just loaded the card name for fun: " + cardName);        
+                return [                  
+                  {
+                    // card detail badges (those that appear on the back of cards)
+                    // also support callback functions so that you can open for example
+                    // open a popup on click
+                    title: "Popup Detail Badge",
+                    text: "Popup",
+                    callback: function(t){
+                        return t.popup({
+                          title: "Hello Dolly",
+                          url: './html/section.html'
+                        });
+                      }
+                  },
+                ];
+              });
+          },
+    // 'card-back-section': function(cbs, options){
+    //     return {
+    //     title: 'My Card Back Section',
+    //     icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+    //     content: {
+    //         type: 'iframe',
+    //         url: cbs.signUrl('./section.html', { anArg: 'hey' }),
+    //         height: 230, // Max height is 1500.
+    //         action: {
+    //             text: 'My Action',
+    //             callback: (t) => t.popup(),
+    //         },
+    //     }
+    //     };
+    // }
           
   });
