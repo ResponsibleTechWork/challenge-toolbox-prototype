ac551e25a33fd77824502db74fb74cabb04978fa
Daniel Hartley <danhartleybcn@gmail.com>
2021-10-31 14:24:53 +0000

    reaons by source

--- src/modals/challenge.js
+++ src/modals/challenge.js
@@ -1,16 +1,46 @@
-const reasons = [
-    'inefficient',
-    'high intensity'
-];
+const sources = [
+    {
+        name: 'Principles of Green Software Engineering',
+        src: 'https://principles.green/',
+        reasons: {
+            positive: [
+                'efficient',
+                'low intensity'
+            ],
+            negative: [
+                'inefficient',
+                'high intensity'
+            ],
+        },
+        selected: true
+    },
+    {
+        name: 'Challenge Toolbox Prototype',
+        src: 'https://www.figma.com/file/E7ZOq83wqAE6ABfoIXnnVm/Challenge-Toolbox-Prototype?node-id=3%3A111',
+        reasons: {
+            positive: [
+                'improves well-being',
+                'improves privacy'
+            ],
+            negative: [
+                'annoying',
+                'misleading'
+            ],
+        },
+        selected: false
+    },
+]
+
+const reasons = sources.find(s => s.selected).negative.reasons;
 
 const reasonsContainer = document.getElementById('reasons');
 
-let items;
+let items = '';
 
 reasons.map(reason => {
-    items =+ `<li>
-                <button class="btn btnChallenge">${reason}</button>
-             </li>`
+    items += `<li>
+                <button id="${reason}" class="btn btnChallenge">${reason}</button>
+             </li>`;
 });
 
 reasonsContainer.innerHTML = items;
