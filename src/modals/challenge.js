2a8a1418003f9ec74430550d1e314b09da8ce764
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-01 07:31:57 +0000

    test parcel immports

--- src/modals/challenge.js
+++ src/modals/challenge.js
@@ -1,58 +1,7 @@
-const sources = [
-    {
-        name: 'Principles of Green Software Engineering',
-        src: 'https://principles.green/',
-        pledges: [
-            {
-                id: 1,
-                text: 'carbon efficient',
-            },            
-            {
-                id: 2,
-                text: 'energy efficient',
-            },            
-        ],
-        reasons: {
-            positive: [
-                'efficient',
-                'low intensity'
-            ],
-            negative: [
-                'inefficient',
-                'high intensity'
-            ],
-        },
-        selected: true
-    },
-    {
-        name: 'Challenge Toolbox Prototype',
-        src: 'https://www.figma.com/file/E7ZOq83wqAE6ABfoIXnnVm/Challenge-Toolbox-Prototype?node-id=3%3A111',
-        pledges: [
-            {
-                id: 1,
-                text: 'respect people',
-            },
-            {
-                id: 2,
-                text: 'protect against abuse',
-            },            
-        ],
-        reasons: {
-            positive: [
-                'improves well-being',
-                'improves privacy'
-            ],
-            negative: [
-                'annoying',
-                'misleading'
-            ],
-        },
-        selected: false
-    },
-]
+import('../shared').then(function (shared) {
 
-const pledges = sources.find(s => s.selected).pledges;
-const reasons = sources.find(s => s.selected).reasons.negative;
+const pledges = shared.sources.find(s => s.selected).pledges;
+const reasons = shared.sources.find(s => s.selected).reasons.negative;
 
 const pledgesContainer = document.getElementById('pledges');
 const reasonsContainer = document.getElementById('reasons');
@@ -103,3 +52,4 @@ const logChallenge = pledge => {
     }
     console.log(pledgesLog);
 };
+});
