a303bcbf702fe0688391a1fdd8d5ee98e928ad95
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-01 10:30:56 +0000

    logChallenge in page

--- src/modals/challenge.js
+++ src/modals/challenge.js
@@ -1,9 +1,28 @@
-import { add, logChallenge } from '../shared/funcs';
+// import { add, logChallenge } from '../shared/funcs';
 import sources from '../shared/sources.json';
 
 const t = window.TrelloPowerUp.iframe();
 const context = t.getContext();
 console.log(logChallenge);
+
+const logChallenge = (context, pledgeId) => {
+    const log = pledgesLog.length > 0 ? pledgesLog.find(log => log.pledge.id === pledgeId) : undefined;
+    if(log === undefined) {
+        pledgesLog.push({
+            type: 'challenge',
+            board: context.board,
+            member: context.member,
+            card: context.card,
+            pledge: {
+                id: pledgeId
+            },        
+        }
+    );
+    } else {
+        pledgesLog = pledgesLog.filter(log => log.pledge.id !== pledgeId);
+    }
+    console.log(pledgesLog);
+};
 
 const pledges = sources.data.find(s => s.selected).pledges;
 const reasons = sources.data.find(s => s.selected).reasons.negative;
---
+++ src/shared/funcs.test.js
@@ -0,0 +1,6 @@
+import { add } from './funcs';
+
+describe('', () => {
+    const result = add(2,2);
+    expect(result).toBe(4);
+});
