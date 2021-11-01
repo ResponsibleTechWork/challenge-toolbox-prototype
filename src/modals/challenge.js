0f7ea92e15d76abab0def44023822bbdcd658c25
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-01 10:01:20 +0000

    onclick event

--- src/modals/challenge.js
+++ src/modals/challenge.js
@@ -1,68 +1,36 @@
-import { add } from '../shared/funcs';
+import { add, logChallenge } from '../shared/funcs';
 import sources from '../shared/sources.json';
-// async function load() {
+
+const t = window.TrelloPowerUp.iframe();
+const context = t.getContext();
+console.log(JSON.stringify(context, null, 2));
+
+const pledges = sources.data.find(s => s.selected).pledges;
+const reasons = sources.data.find(s => s.selected).reasons.negative;
+
+const pledgesContainer = document.getElementById('pledges');
+const reasonsContainer = document.getElementById('reasons');
+
+const redrawChallengePledges = () => {
+    const pledgeItems = pledges.map(pledge => {    
+        return `<li>
+                    <button id="${pledge.id}" class="btn btnChallenge" onclick="logChallenge('${pledge.id}');">${pledge.text}</button>
+                </li>`;
+    });
+
+    pledgesContainer.innerHTML = pledgeItems.join('');
+};
+
+redrawChallengePledges();
+
+const redrawChallengeReasons = () => {
+    const reasonItems = reasons.map(reason => {
+        return `<li>
+                    <button id="${reason}" class="btn btnChallenge">${reason}</button>
+                </li>`;
+    });
     
-        // const sources = await import('../shared/sources.json');
-        // const shared = await import('../shared/funcs');
-        
-        console.log(sources.data);
-        console.log('add: ', add);
-        // console.log('shared: ', shared);
+    reasonsContainer.innerHTML = reasonItems.join('');
+};
 
-        const pledges = sources.data.find(s => s.selected).pledges;
-        const reasons = sources.data.find(s => s.selected).reasons.negative;
-
-        const pledgesContainer = document.getElementById('pledges');
-        const reasonsContainer = document.getElementById('reasons');
-
-        const redrawChallengePledges = () => {
-            const pledgeItems = pledges.map(pledge => {    
-                return `<li>
-                            <button id="${pledge.id}" class="btn btnChallenge" onclick="logChallenge(${pledge.id});">${pledge.text}</button>
-                        </li>`;
-            });
-
-            pledgesContainer.innerHTML = pledgeItems.join('');
-        };
-
-        redrawChallengePledges();
-
-        const redrawChallengeReasons = () => {
-            const reasonItems = reasons.map(reason => {
-                return `<li>
-                            <button id="${reason}" class="btn btnChallenge">${reason}</button>
-                        </li>`;
-            });
-            
-            reasonsContainer.innerHTML = reasonItems.join('');
-        };
-
-        redrawChallengeReasons();
-
-        const t = window.TrelloPowerUp.iframe();
-        const context = t.getContext();
-        console.log(JSON.stringify(context, null, 2));
-
-        let pledgesLog = [];
-
-        const logChallenge = pledgeId => {
-            const log = pledgesLog.find(log => log.pledge.id === pledgeId);
-            if(log === undefined) {
-                pledgesLog.push({
-                    type: 'challenge',
-                    board: context.board,
-                    member: context.member,
-                    card: context.card,
-                    pledgeId: pledgeId,        
-                }
-            );
-            } else {
-                pledgesLog = pledgesLog.filter(log => log.pledge.id !== pledgeId);
-            }
-            console.log(pledgesLog);
-        };
-// }
-
-// load();
-
-// });
+redrawChallengeReasons();
--- src/shared/funcs.js
+++ src/shared/funcs.js
@@ -1,3 +1,23 @@
 export const add = (x,y) => {
     return x + y;
 };
+
+
+let pledgesLog = [];
+
+export const logChallenge = pledgeId => {
+    const log = pledgesLog.find(log => log.pledge.id === pledgeId);
+    if(log === undefined) {
+        pledgesLog.push({
+            type: 'challenge',
+            board: context.board,
+            member: context.member,
+            card: context.card,
+            pledgeId: pledgeId,        
+        }
+    );
+    } else {
+        pledgesLog = pledgesLog.filter(log => log.pledge.id !== pledgeId);
+    }
+    console.log(pledgesLog);
+};
