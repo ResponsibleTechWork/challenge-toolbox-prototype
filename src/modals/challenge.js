64c7978e93ddabaecfd1c5f423e671667856775e
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-01 12:18:25 +0000

    build buttons programaticallly

--- src/modals/challenge.js
+++ src/modals/challenge.js
@@ -9,7 +9,7 @@ const reasonsContainer = document.getElementById('reasons');
 
 let pledgesLog = [];
 
-const clickHandler = pledgeId => {
+const clickHandler = (e, pledgeId) => {
     const t = window.TrelloPowerUp.iframe();
     const context = t.getContext();
     pledgesLog = logChallenge(pledgesLog, context, pledgeId);
@@ -17,12 +17,27 @@ const clickHandler = pledgeId => {
 
 const redrawChallengePledges = () => {
     const pledgeItems = pledges.map(pledge => {    
-        return `<li>
-                    <button id="${pledge.id}" class="btn btnChallenge" onclick="clickHandler(${pledge.id})">${pledge.text}</button>
-                </li>`;
+        const li = document.createElement('li');
+        const btn = document.createElement('button');
+              btn.id = pledge.id;
+              btn.classList.add('btn'); 
+              btn.classList.add('btnChallenge'); 
+              btn.innerHTML = pledge.text;
+              btn.addEventListener('click', e => clickHandler(e, pledge.id));
+              li.appendChild(btn);
+              return li;
+        // return `<li>
+        //             <button id="${pledge.id}" class="btn btnChallenge" onclick="clickHandler(${pledge.id})">${pledge.text}</button>
+        //         </li>`;
+        // return `<li>
+        //             <button id="${pledge.id}" class="btn btnChallenge" onclick="clickHandler(${pledge.id})">${pledge.text}</button>
+        //         </li>`;
     });
 
-    pledgesContainer.innerHTML = pledgeItems.join('');
+    // pledgesContainer.innerHTML = pledgeItems.join('');
+    pledgeItems.forEach(item => {
+        pledgesContainer.appendChild(item);
+    });
 };
 
 redrawChallengePledges();
