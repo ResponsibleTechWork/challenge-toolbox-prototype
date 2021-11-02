7c9d1a5ecf7dee6b08afb83cb47b40de39be69a6
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-02 13:13:10 +0000

    getCard checks

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,27 +1,28 @@
 const getCard = async id => {
 
-    var requestSuccess = function (data) {
+    console.log('card id: ', id);
+
+    const success = data => {
         console.log('Card returned successfully.');
         console.log(JSON.stringify(data, null, 2));
     };
 
-    return await window.Trello.get('/cards/', id, requestSuccess);
+    const error = response => {
+        console.log('error: ', response);
+    };
 
-    // const response = await fetch(url, {
-    //     method: 'GET', // GET, POST, PUT, DELETE, etc.
-    //     mode: 'cors', // no-cors, *cors, same-origin
-    //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //     credentials: 'same-origin', // include, *same-origin, omit
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //       // 'Content-Type': 'application/x-www-form-urlencoded',
-    //     },
-    //     redirect: 'follow', // manual, *follow, error
-    //     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    //     body: JSON.stringify(data) // body data type must match "Content-Type" header
-    //   });
-    //   return response.json();
+    console.log('window.Trello: ', window.Trello);
 
+    let response;
+
+    try {
+        response = await window.Trello.get('/cards/', id, success, error);
+    } catch(e) {
+        console.error('error: ', e.message);
+        console.warn('reponse: ', response);
+    }
+
+    return response;
 };
 
 const api = {
