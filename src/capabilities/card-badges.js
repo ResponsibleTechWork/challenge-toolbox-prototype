3eed20200e16631c6f4c6a5c8f7163402bf43a95
Daniel Hartley <danhartleybcn@gmail.com>
2021-11-05 18:49:51 +0000

    private visibility

--- src/capabilities/card-badges.js
+++ src/capabilities/card-badges.js
@@ -5,7 +5,7 @@ import { ChallengeLog } from '../shared/challenge-log';
 const get = async t => {
 
     const scope = enums.Scope.Card;
-    const visibility = enums.Visibility.Shared;
+    const visibility = enums.Visibility.Private;
     const key = enums.Key.LogEntries;
     const capability = enums.Capability.CardBadges;
 
