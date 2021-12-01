e62f247c60ec3fd0058d4b8bf8edb3bf4df46702
Daniel Hartley <danhartleybcn@gmail.com>
2021-12-01 13:11:37 +0000

    test board icons

--- src/capabilities/board-buttons.js
+++ src/capabilities/board-buttons.js
@@ -4,7 +4,7 @@ import ChallengeLog from '../shared/challenge-log';
 
 import data from '../api/data/authors.json';
 
-const WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 const BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
 const get = async t => {
@@ -17,11 +17,12 @@ const get = async t => {
     const onBtnClick = async t => {
 
         const prefs = await t.get(trelloEnums.Scope.Board, trelloEnums.Visibility.Shared, trelloEnums.Key.ChallengePreferences);
+        const selected = data.authors.find(a => a.id === prefs.id);
         
         const preferences = data.authors.map(a => {
             return {
                 text: a.title,
-                icon: WHITE_ICON,
+                icon: selected.id === prefs.id ? BLACK_ICON : GRAY_ICON,
                 callback: async (t, opts) => {
                     await t.set(scope, visibility, key, a);
                     const response = await t.get(scope, visibility, key);
