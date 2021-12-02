7f5f6e6678c863f285f9f03a2fb3d2249235d9da
Daniel Hartley <danhartleybcn@gmail.com>
2021-12-02 19:33:17 +0000

    wire up card back section

--- src/modals/card-back-modal-challenges.js
+++ src/modals/card-back-modal-challenges.js
@@ -1,3 +1,6 @@
+import trelloEnums from '../shared/trello-enums';
+import ChallengeLog from '../shared/challenge-log';
+
 const init = async () => {
 
     const isLive = document.referrer === 'https://trello.com/';
@@ -13,6 +16,49 @@ const init = async () => {
     } else {
         data = {}
     }
+
+    const prompt = document.getElementById('prompt');
+    const labelsContainer = document.getElementById('labels');
+
+    const challengeLog = new ChallengeLog(type);
+
+    const capability = data.capabilites.find(c => c.capability === trelloEnums.Capability.CardDetailBadges);
+
+    const clickLabelHandler = async (e, id) => {
+
+        const context = isLive ? t.getContext() : {
+            board: 'board #1',
+            member: 'member #1',
+            card: 'card #1',
+        };
+
+        const label = labels.find(label => parseInt(label.id) === parseInt(id));
+
+        const { isPledgeNowLogged, updatedPledges } = challengeLog.togglePledge(context, label);
+
+        const scope = trelloEnums.Scope.Card;
+        const visibility = trelloEnums.Visibility.Shared;
+        const key = trelloEnums.Key.LogEntries;
+
+        await t.set(scope, visibility, key, updatedPledges);
+
+        const response = await t.get(scope, visibility, key);
+
+        console.log(response);
+    };
+
+    const labels = data.labels.map(label => {
+        return `<li>
+                    <button id="${label.id}" class="btn">${label.text}</button>
+                </li>`;
+    });
+
+    prompt.innerText = capability.prompt;
+    labelsContainer.innerHTML = labels.join('');
+
+    labelsContainer.querySelectorAll('.btn').forEach(btn => {
+        btn.addEventListener('click', e => clickLabelHandler(e, btn.id));
+    });
 
 };
 
