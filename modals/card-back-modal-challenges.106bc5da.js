5254d0a56b1cfabcb5cb41bdc9c42cfe83ff9583
Daniel Hartley <danhartleybcn@gmail.com>
2021-12-02 19:33:17 +0000

    wire up card back section

--- modals/card-back-modal-challenges.106bc5da.js
+++
@@ -1,2 +0,0 @@
-(async()=>{let e,o;"https://trello.com/"===document.referrer?(e=window.TrelloPowerUp.iframe(),o=e.arg("data"),console.log(o)):o={}})();
-//# sourceMappingURL=card-back-modal-challenges.106bc5da.js.map
